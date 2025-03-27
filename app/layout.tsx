96934765fcabada57197642fb8c0d4b9994683ca
kafou135 <youssefkafou@gmail.com>
2025-03-27 18:32:33 +0000

    Update website

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,8 +25,21 @@ export default function RootLayout({
        <meta name="google-adsense-account" content="ca-pub-8853506957457177"/>
        <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8853506957457177"
      crossOrigin="anonymous"></Script>
+     <Script async custom-element="amp-ad" src="https://cdn.ampproject.org/v0/amp-ad-0.1.js"></Script>
       </head>
-      <body className={inter.className}>
+      <body>
+
+      <Script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-8853506957457177"
+     crossOrigin="anonymous"></Script>
+<ins id="adsbygoogle"
+     className="display:block"
+     data-ad-client="ca-pub-8853506957457177"
+     data-ad-slot="8568517476"
+     data-ad-format="auto"
+     data-full-width-responsive="true"></ins>
+<Script>
+     (adsbygoogle = window.adsbygoogle || []).push({});
+</Script>
         {/* Simple Background */}
         <div className="min-h-screen bg-gray-900"> {/* Changed to solid dark background */}
           {/* Content */}
